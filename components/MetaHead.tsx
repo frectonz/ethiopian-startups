4d84f4300811ea87e42e639ae8b8938576b6226c
fraol0912 <fraol0912@gmail.com>
2021-11-12 16:26:39 +0300

    add og fields to head

--- components/MetaHead.tsx
+++ components/MetaHead.tsx
@@ -11,6 +11,13 @@ const MetaHead: React.FunctionComponent<MetaHeadProps> = ({ title = "" }) => {
       <title>Ethiopian StartUps {title ? ` - ${title}` : ""}</title>
       <meta name="description" content="The big list of Ethiopian startups." />
       <link rel="icon" type="image/png" href="/ethiopic.png" />
+
+      <meta property="og:title" content="Ethiopian Startups" />
+      <meta
+        property="og:description"
+        content="The big list of ethiopian startups."
+      />
+      <meta property="og:image" content="/ethiopic_bg.png" />
     </Head>
   );
 };
