2dc5b2f3115e24f544a09aeeb6d8cc3b9d09f417
fraol0912 <fraol0912@gmail.com>
2021-12-03 14:21:06 +0300

    installable pwa

--- components/MetaHead.tsx
+++ components/MetaHead.tsx
@@ -1,16 +1,21 @@
 import Head from "next/head";
-import * as React from "react";
+import { FunctionComponent } from "react";
 
 interface MetaHeadProps {
   title?: string;
 }
 
-const MetaHead: React.FunctionComponent<MetaHeadProps> = ({ title = "" }) => {
+const MetaHead: FunctionComponent<MetaHeadProps> = ({ title = "" }) => {
   return (
     <Head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+
       <title>Ethiopian StartUps {title ? ` - ${title}` : ""}</title>
+
       <meta name="description" content="The big list of Ethiopian startups." />
-      <link rel="icon" type="image/ico" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
 
       <meta property="og:title" content="Ethiopian Startups" />
       <meta
@@ -18,6 +23,12 @@ const MetaHead: React.FunctionComponent<MetaHeadProps> = ({ title = "" }) => {
         content="The big list of ethiopian startups."
       />
       <meta property="og:image" content="/ethiopic_bg.png" />
+      <meta
+        name="keywords"
+        content="startups,ethiopia,ethiopian,startup list,ethiopian incubators,ethiopian startups,ethiopian hubs"
+      />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#e5e7eb" />
     </Head>
   );
 };
